c9bd42a023f852384f8f9ea6dfc3f228ec3283b5
EEVEEzz <ivan@kotze.co.za>
2021-02-05 14:39:49 +0200

    asd

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://user:n8w134xknVAhYeRy@cluster0.wmcvu.mongodb.net/mybrary?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
