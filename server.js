05251ef398b5e523b772670a6576049947497dfc
EEVEEzz <ivan@kotze.co.za>
2021-02-05 17:43:34 +0200

    dev commit

--- server.js
+++ server.js
@@ -1,32 +1,33 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const expressLayouts = require('express-ejs-layouts');
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load()
+}
 
-const indexRouter = require('./routes/index');
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
-app.set('view engine', 'ejs');
-app.set('views', __dirname + "/views");
-app.set('layout', 'layouts/layout');
-app.use(expressLayouts);
-app.use(express.static('public'));
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://user:n8w134xknVAhYeRy@cluster0.wmcvu.mongodb.net/mybrary?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(methodOverride('_method'))
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
-const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-db.on('error', (error) => console.error(error));
-db.once('open', () => console.log("Connected to Mongoose"));
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 
+app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
-app.use('/', indexRouter);
-
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000)
